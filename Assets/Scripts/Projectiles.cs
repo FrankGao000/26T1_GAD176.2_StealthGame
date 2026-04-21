e1b726d38c565890fb841e642ade3e219ccc4c90
Ithiel Kwong - Home base <1034813@student.sae.edu.au>
2026-04-22 00:20:24 +1000

    Completed all turret's data and bullet stats

--- Assets/Scripts/Projectiles.cs
+++ Assets/Scripts/Projectiles.cs
@@ -19,11 +19,15 @@ public class Projectiles : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
-        Debug.Log($"HIHI NOTHING BUT I HIT {other.gameObject.name} and my parent is {transform.parent}");
-        if((other.gameObject.layer == LayerMask.NameToLayer("Player") || other.gameObject.layer == LayerMask.NameToLayer("Enemy")) && other.gameObject.transform != transform.parent)
+        Debug.Log($"Entering fire range!!!");
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player") && other.gameObject.transform != transform.parent)
         {
             
           Debug.Log("You've been Hit!");
+          if(other.gameObject.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.TakeDamage(damage);
+            }
         
         }
         if(other.transform.root.gameObject != transform.root.gameObject && other.gameObject.layer != LayerMask.NameToLayer("Environment"))
